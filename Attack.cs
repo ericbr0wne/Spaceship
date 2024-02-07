781b42f1edb8eda5ca582dfc592435a3b67a4bd1
Benny.Ahlin <Benny.Ahlin@hotmail.com>
2024-02-07 11:09:53 +0100

    added classes to work in

---
+++ Attack.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spaceships;
+
+public class Attack
+{
+
+    public void Damage() //lose hp
+    {
+
+
+
+    }
+
+    public void Result() //hit or miss?
+    {
+
+
+
+    }
+
+
+
+
+}
+
---
+++ User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spaceships;
+
+public class User
+{
+
+    public void Name()
+    {
+
+
+
+    }
+
+
+    public void Position()
+    {
+
+
+
+    }
+
+
+
+}
