b817e1d3cc4d1696b716090cd812bcf7aa35d240
unknown <ericbrowne@hotmail.com>
2024-02-09 15:45:21 +0100

    cleaned up the code

--- Program.cs
+++ Program.cs
@@ -5,13 +5,9 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.Net;
 using System.Text;
 
-
-//database goes here 
 string dbUri = "Host=localhost;Port=5455;Username=postgres;Password=postgres;Database=spaceship";
 await using var db = NpgsqlDataSource.Create(dbUri);
 
-
-//cancel server
 bool listen = true;
 Console.CancelKeyPress += delegate (object? sender, ConsoleCancelEventArgs e)
 {
@@ -20,76 +16,58 @@ Console.CancelKeyPress += delegate (object? sender, ConsoleCancelEventArgs e)
     listen = false;
 };
 
-
-//create listener
 int port = 3000;
 HttpListener listener = new();
-listener.Prefixes.Add($"http://localhost:{port}/"); // <host> kan t.ex. vara 127.0.0.1, 0.0.0.0, ...
+listener.Prefixes.Add($"http://localhost:{port}/"); 
 
-//start listener
 try
 {
     listener.Start();
     listener.BeginGetContext(new AsyncCallback(HandleRequest), listener);  //Här körs resten av kod i Async /wrapper 
     Console.WriteLine("Server listening on port: " + port);
     while (listen) { };
-
 }
-//stop listener
 finally
 {
     listener.Stop();
 }
 
-
-//take request and goes to Router
 void HandleRequest(IAsyncResult result)
 {
     if (result.AsyncState is HttpListener listener)
     {
         HttpListenerContext context = listener.EndGetContext(result);
         Router(context);
-        HttpListenerRequest request = context.Request;
 
-        HttpListenerResponse response = context.Response;
-
-        Console.WriteLine($"{request.HttpMethod} request received");
         listener.BeginGetContext(new AsyncCallback(HandleRequest), listener);
 
     }
 }
 
-
 void Router(HttpListenerContext context)
 {
-    // string message = ""; //fill with responde messages to Client
-
     HttpListenerRequest request = context.Request;
     HttpListenerResponse response = context.Response;
+    Console.WriteLine($"{request.HttpMethod} request received");
+
     switch (request.HttpMethod, request.Url?.AbsolutePath) // == endpoint
     {
-        case ("GET", "/"):
+        case ("GET", "/users"):
             RootGet(response);
             break;
         case ("POST", "/post/user"):
-            // first check then post what u wanted
             RootPost(request, response);
             break;
         default:
             NotFound(response);
             break;
     }
-    // Send the response back to the client
-    //byte[] buffer = Encoding.UTF8.GetBytes(message);
-
-    //response.OutputStream.Write(buffer, 0, buffer.Length);
-    //response.OutputStream.Close();
 }
 
 void RootGet(HttpListenerResponse response)
 {
-    string message = ""; //fill with responde messages to Client
-    //string responseString = "";
+
+    string message = ""; 
     const string getUsers = "select * from users";
     var cmd = db.CreateCommand(getUsers);
     var reader = cmd.ExecuteReader();
@@ -105,7 +83,7 @@ void RootGet(HttpListenerResponse response)
         message += reader.GetInt32(2) + ", "; // hp
 
     }
-    //await Console.Out.WriteLineAsync(responseString);
+
     byte[] buffer = Encoding.UTF8.GetBytes(message);
     response.OutputStream.Write(buffer, 0, buffer.Length);
     response.OutputStream.Close();
@@ -113,9 +91,11 @@ void RootGet(HttpListenerResponse response)
 
 void RootPost(HttpListenerRequest req, HttpListenerResponse res)
 {
+    //curl -d "user=eric" -X POST http://localhost:3000/post/user
+
     StreamReader reader = new(req.InputStream, req.ContentEncoding);
     var cmd = db.CreateCommand("insert into users (name) values ($1) RETURNING id");
-    //curl -d "user=eric" -X POST http://localhost:3000/post/user
+    
     string postBody = reader.ReadToEnd();
     Console.WriteLine(postBody);
 
